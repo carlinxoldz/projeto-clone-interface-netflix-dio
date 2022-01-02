11d4413767c364c0f3d897b582a3ad5768a4f778
calinxoldz <carlinxold@hotmail.com>
2022-01-02 12:46:14 -0300

    3

---
+++ js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      item: 1
+    },
+    600: {
+      item: 3
+    },
+    1000: {
+      item: 5
+    }
+  }
+})
